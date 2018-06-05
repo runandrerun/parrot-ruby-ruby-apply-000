ee8606a262112a2a47f9aab2413d0b356f3d253a
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-05 00:38:10 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,14 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase, squawk = "Squawk!")
-  if phrase != String
-    puts "#{squawk}"
-  elsif parrot 
-    puts "#{squawk}"
-  else
-    puts "#{phrase}"
-  end
+def parrot(squawk = "Squawk!")
 end
 
 
